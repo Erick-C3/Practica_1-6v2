e8a0561cf4d3a2759833ba0982d64c4f1770c7da
Erick <erick.nahuelpaiva.001@gmail.com>
2023-03-31 10:47:23 -0300

    primer parte 1-6 terminado

---
+++ algoritmo.js
@@ -0,0 +1,5 @@
+let nombre = prompt("Como te llamas?");
+
+console.log( nombre );
+document.querySelector("body").innerHTML = "<h1> "+ nombre + " </h1>";
+document.write("<h2> Mi nombre es "+ nombre + " </h2>");
